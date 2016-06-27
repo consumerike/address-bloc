97b89f1827c159221caa58629963517679c8b39c
consumerike <consumerike@gmail.com>
2016-06-27 00:39:00 +0000

    checkpoint20 complete

---
+++ controllers/address_bloc.rb
@@ -0,0 +1,11 @@
+require_relative '../controllers/menu_controller'
+ 
+ puts "Welcome to AddressBloc!"
+
+  menu = MenuController.new
+
+    system "clear"
+    puts "Welcome to AddressBloc!"
+
+ return menu.main_menu
+ 
